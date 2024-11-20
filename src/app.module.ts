493fbfe9a933a99ffd7dadb168c04ffb04c2ab8d
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2024-11-20 15:47:01 +0200

    imported chatmodules and message modules in it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { typeormconfig } from 'typeOrmConfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
+//import { HouseModule } from './house/house.module';
+//import { UserModule } from './user/user.module';
+import { messageModule } from './message/message.module';
 import { ChatGateway } from './chat/chat.gateway';
 
+
+
 @Module({
-  imports: [],
+  imports: [ChatModule,TypeOrmModule.forRoot(typeormconfig), ChatGateway,messageModule],
   controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  providers: [AppService],
+  exports:[messageModule],
+
 })
 export class AppModule {}
