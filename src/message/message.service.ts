e8ce8bdb1bd869bb56a6b91fb6dd03f8287e2762
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2024-11-20 15:42:59 +0200

    added message service that handles business logic

---
+++ src/message/message.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { message } from './entities/message.entity';
+
+
+@Injectable()
+export class MessageService {
+
+  constructor(
+    @InjectRepository(message)
+    private readonly messagesRepository:Repository<message>
+  ){}
+  create(createMessageDto: CreateMessageDto) {
+  
+    return  this.messagesRepository.save(createMessageDto);
+  }
+
+  findAll() {
+    return this.messagesRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.messagesRepository.findOne({});
+  }
+
+  update(id: number, updateMessageDto: UpdateMessageDto) {
+    return this.messagesRepository.update(id,{});
+  }
+
+  remove(id: number) {
+    return this.messagesRepository.delete(id);
+  }
+}
