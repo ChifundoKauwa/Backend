66e649e9370003bc17c77d71bd8afa2f00e07353
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2024-11-22 18:07:23 +0200

    added message module

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -3,11 +3,12 @@ import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import {message} from './entities/message.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatGateway } from 'src/chat/chat.gateway';
 
 @Module({
   imports:[TypeOrmModule.forFeature([message])],
   controllers: [MessageController],
-  providers: [MessageService],
+  providers: [MessageService,ChatGateway],
   exports:[MessageService]
 })
 export class messageModule {}
