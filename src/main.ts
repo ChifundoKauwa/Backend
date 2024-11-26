fae5cceabf79d4c0c9fb832b012c9b9f002ac6bd
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2024-11-26 12:12:26 +0200

    added netlify files for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
-const server =express();
- export const handler = async(req:any,res:any)=>{
-  const app =await NestFactory.create(AppModule,new ExpressAdapter(server));
-  app.setGlobalPrefix('api');
-  server(req,res);
- };
+ async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
+};
+bootstrap();
