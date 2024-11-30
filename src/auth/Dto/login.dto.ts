d309c5605ddb83949e5b85de019b501c699e60b8
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2024-11-30 13:10:57 +0200

    chnged creat user dto

--- src/auth/Dto/login.dto.ts
+++ src/auth/Dto/login.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @IsEmail()
-  email: string;
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
 
-  @IsNotEmpty()
-  password: string;
+    @IsNotEmpty()
+    @MinLength(6)
+    password: string;
 }
