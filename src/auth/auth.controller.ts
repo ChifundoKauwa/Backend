45dc00a2bddbaa789a76dad83280177b4cb9229c
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2024-11-30 12:05:16 +0200

    corrected the login dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginDto } from './dto/login.dto';
+import { LoginDto } from './Dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
